a132675af53160fa962f49161f0124ca9575b9e2
Nickolas J. Wilson <nickolas.j.wilson@gmail.com>
2024-06-24 20:51:29 -0500

    refactor: store the glob pattern for the Python files in a constant

--- tasks.py
+++ tasks.py
@@ -2,6 +2,8 @@
 
 import invoke
 
+_ALL_PY_FILES = "*.py test"
+
 
 @invoke.task
 def test(context: invoke.context.Context) -> None:
@@ -12,17 +14,17 @@ def test(context: invoke.context.Context) -> None:
 @invoke.task
 def check(context: invoke.context.Context) -> None:
     """Statically checks the types."""
-    context.run("mypy *.py test")
+    context.run(f"mypy {_ALL_PY_FILES}")
 
 
 @invoke.task
 def format_(context: invoke.context.Context) -> None:
     """Formats the code."""
-    context.run("isort *.py test")
-    context.run("black *.py test")
+    context.run(f"isort {_ALL_PY_FILES}")
+    context.run(f"black {_ALL_PY_FILES}")
 
 
 @invoke.task
 def analyze(context: invoke.context.Context) -> None:
     """Statically analyzes the code."""
-    context.run("pylint *.py test")
+    context.run(f"pylint {_ALL_PY_FILES}")
